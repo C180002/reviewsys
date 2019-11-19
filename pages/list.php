9a7e01348a97472c97032bde23594aa15f9794ec
Hiro281 <44799467+Hiro281@users.noreply.github.com>
2019-11-19 15:03:25 +0900

    課題２終わり
    コメント削除

--- pages/list.php
+++ pages/list.php
@@ -22,7 +22,6 @@
     
     $restaurant = new Restaurant();
     
-    // $restaurant = array('id' => '1', 'name' => 'Wine Bar ENOTECA', 'area_id' => '3', 'summary' => '常時10種類以上の赤・白ワインをご用意しています。<br />記念日にご来店ください。');
     $restaurant->setId('1');
     $restaurant->setName('Wine Bar ENOTECA');
     $restaurant->setAreaId('3');
@@ -32,7 +31,6 @@
 
     $restaurant = new Restaurant();
     
-    // $restaurant = array('id' => '2', 'name' => 'スペイン料理 ポルファボール！', 'area_id' => '2', 'summary' => '味が自慢。スペイン現地で学んだシェフが出す味は本物です。');
     $restaurant->setId('2');
     $restaurant->setName('スペイン料理 ポルファボール！');
     $restaurant->setAreaId('2');
@@ -42,7 +40,6 @@
     
     $restaurant = new Restaurant();
     
-    // $restaurant = array('id' => '3', 'name' => 'パス・パスタ', 'area_id' => '3', 'summary' => '本当のパスタを味わうならパス・パスタで！<br />休日の優雅なランチタイムに是非どうぞ。');
     $restaurant->setId('3');
     $restaurant->setName('パス・パスタ');
     $restaurant->setAreaId('3');
@@ -52,7 +49,6 @@
     
     $restaurant = new Restaurant();
     
-    // $restaurant = array('id' => '4', 'name' => 'レストラン「有閑」', 'area_id' => '2', 'summary' => '広い店内で、お昼の優雅なひと時を過ごしませんか？');
     $restaurant->setId('4');
     $restaurant->setName('レストラン「有閑」');
     $restaurant->setAreaId('2');
@@ -62,7 +58,6 @@
     
     $restaurant = new Restaurant();
     
-    // $restaurant = array('id' => '5', 'name' => 'ビストロ「ルーヴル」', 'area_id' => '3', 'summary' => '高層ビル42階のビストロで、県内が一望できる。恋人とのひと時をここで過ごしませんか。');
     $restaurant->setId('5');
     $restaurant->setName('ビストロ「ルーヴル」');
     $restaurant->setAreaId('3');
@@ -72,7 +67,6 @@
     
     $restaurant = new Restaurant();
     
-    // $restaurant = array('id' => '6', 'name' => '海沿いのレストラン La Mer', 'area_id' => '1', 'summary' => '海が見える、海沿いのレストランです。');
     $restaurant->setId('6');
     $restaurant->setName('海沿いのレストラン La Mer');
     $restaurant->setAreaId('1');
@@ -82,7 +76,6 @@
     
     $restaurant = new Restaurant();
     
-    // $restaurant = array('id' => '7', 'name' => 'レストラン さくら', 'area_id' => '3', 'summary' => '四季折々の自然を楽しむ伊豆市に、ひっそりと佇む隠れ家レストラン。\n旅行でいらっしゃった方も、お近くの方も、お気軽にお立ち寄りください。');
     $restaurant->setId('7');
     $restaurant->setName('レストラン さくら');
     $restaurant->setAreaId('3');
@@ -134,13 +127,9 @@
 <?php
     foreach ($restaurant_list as $rstrnt)
     {
-        // $id = $rstrnt["id"];
         $id = $rstrnt->getId();
-        // $name = $rstrnt["name"];
         $name = $rstrnt->getName();
-        // $area_id = $rstrnt["area_id"];
         $area_id = $rstrnt->getAreaId();
-        // $summary = $rstrnt["summary"];
         $summary = $rstrnt->getSummary();
 
         if ($area_id_param !== '')
